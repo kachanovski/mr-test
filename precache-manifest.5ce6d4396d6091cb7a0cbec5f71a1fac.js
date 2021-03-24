d24a59791a04cb49bd6d03aaf627110293d80e03
kachanovski03@gmail.com <13shurik03>
2021-03-24 16:32:16 +0300

    Updates

---
+++ precache-manifest.5ce6d4396d6091cb7a0cbec5f71a1fac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056aa0edd55578565a04007b1beea34a",
+    "url": "/mr-test/index.html"
+  },
+  {
+    "revision": "201877d758e956482871",
+    "url": "/mr-test/static/css/main.43075a2b.chunk.css"
+  },
+  {
+    "revision": "e29e0c8a7774750519f4",
+    "url": "/mr-test/static/js/2.e0851c6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mr-test/static/js/2.e0851c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201877d758e956482871",
+    "url": "/mr-test/static/js/main.2d338fbe.chunk.js"
+  },
+  {
+    "revision": "9d62de4edbc35c71a16c",
+    "url": "/mr-test/static/js/runtime-main.0bdc8713.js"
+  }
+]);
